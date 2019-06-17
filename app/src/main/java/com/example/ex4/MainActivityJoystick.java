49538fab9ac81e5128564ce06244da710423dc93
Noam Cohen <noam.cohen4@live.biu.ac.il>
2019-06-17 19:52:25 +0300

    Zehu.

--- app/src/main/java/com/example/ex4/MainActivityJoystick.java
+++ app/src/main/java/com/example/ex4/MainActivityJoystick.java
@@ -17,7 +17,7 @@ public class MainActivityJoystick extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_joystick);
-
+// not necessary
 //        textView1 = (TextView) findViewById(R.id.textView1);
 //        textView2 = (TextView) findViewById(R.id.textView2);
 //        textView3 = (TextView) findViewById(R.id.textView3);
